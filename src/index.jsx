bc297376ace58e70a7aee06d526c64617aaa5aa3
Justin Baum <justin.baum@ninefx.com>
2020-01-08 15:43:38 -0500

    Up again

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 const title = 'React with Webpack and Babel';
+
+let App = document.getElementById('app')
+
 ReactDOM.render(
   <div>{title}</div>,
-  document.getElementById('app')
-);
+  App);
 module.hot.accept();
